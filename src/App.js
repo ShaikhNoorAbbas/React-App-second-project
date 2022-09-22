bb14e9d79499233df738ce56e9376fc21a74c74b
shaikh noor abbas <shaikhnoor1512@gmail.com>
2022-09-22 20:42:34 +0530

    changes made in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './Components/Login/Login';
 import Home from "./Components/Home/Home";
@@ -7,14 +7,26 @@ import MainHeader from './Components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    console.log("Rendered Outside " + isLoggedIn);
+    const store = localStorage.getItem('login');
+    if (store === '1') {
+      setIsLoggedIn(true);  
+      console.log("Rendered inside");
+    };
+    return () => { console.log("before") };
+  }, [isLoggedIn]);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('login', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('login');
   };
 
   return (
